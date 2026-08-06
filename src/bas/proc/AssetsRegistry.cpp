e693708d4fd9f8b9b1cf097c228c1f2a1695a488
Lenik <pub.lenik@bodz.net>
2026-08-06 16:50:01 +0800

    refactor(proc): move AssetsRegistry implementation out of the header
    Keep AssetsRegistry.hpp as declarations only and place instance/pushLayer
    in AssetsRegistry.cpp so the overlay registry is a normal translation
    unit (already wired in meson.build).

--- src/bas/proc/AssetsRegistry.cpp
+++ src/bas/proc/AssetsRegistry.cpp
@@ -1,1 +1,21 @@
 #include "AssetsRegistry.hpp"
+
+#include <stdexcept>
+#include <vector>
+
+std::unique_ptr<OverlayVolume>& AssetsRegistry::instance() {
+    static std::unique_ptr<OverlayVolume> instance;
+    return instance;
+}
+
+void AssetsRegistry::pushLayer(Volume* vol) {
+    if (vol == nullptr) {
+        throw std::invalid_argument("Volume cannot be nullptr");
+    }
+    auto& inst = instance();
+    if (!inst) {
+        inst = std::make_unique<OverlayVolume>("", std::vector<Volume*>{vol});
+    } else {
+        inst->pushLayer(vol);
+    }
+}
--- src/bas/proc/AssetsRegistry.hpp
+++ src/bas/proc/AssetsRegistry.hpp
@@ -4,26 +4,11 @@
 #include "../volume/OverlayVolume.hpp"
 
 #include <memory>
-#include <stdexcept>
-#include <vector>
 
 class AssetsRegistry {
-public:
-    static std::unique_ptr<OverlayVolume>& instance() {
-        static std::unique_ptr<OverlayVolume> instance;
-        return instance;
-    }
-    static void pushLayer(Volume* vol) {
-        if (vol == nullptr) {
-            throw std::invalid_argument("Volume cannot be nullptr");
-        }
-        auto& inst = instance();
-        if (!inst) {
-            inst = std::make_unique<OverlayVolume>("", std::vector<Volume*>{vol});
-        } else {
-            inst->pushLayer(vol);
-        }
-    }
+  public:
+    static std::unique_ptr<OverlayVolume>& instance();
+    static void pushLayer(Volume* vol);
 };
 
 #endif // ASSETSREGISTRY_HPP
